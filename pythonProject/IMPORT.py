567e870f85a5a9faf7496ba7dc6bd57ff5101964
rudolfkopytov <107266711+rudolfkopytov@users.noreply.github.com>
2022-07-08 13:34:40 +0300

    Add files via upload

---
+++ pythonProject/IMPORT.py
@@ -0,0 +1,2 @@
+import privet
+privet. privet()
---
+++ pythonProject/moduls.py
@@ -0,0 +1,4 @@
+import math as m
+print(m.factorial(10))# вызов всей функции
+from math import factorial as fa
+print(fa(10)) # - вызов фрагмента функции, необходимой мне.
